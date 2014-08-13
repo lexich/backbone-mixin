c9ffa0468b4c47178d69d17aa3b59775a5306d66
Efremov Alexey <alexey.efremov@saltpepper.ru>
2014-08-13 11:05:21 +0400

    update to 0.3.3

--- build/backbone-mixin.js
+++ build/backbone-mixin.js
@@ -1,7 +1,7 @@
 var MixinBackbone;
 
 MixinBackbone = function(Backbone) {
-  MixinBackbone.version = "0.3.2";
+  MixinBackbone.version = "0.3.3";
   MixinBackbone = function(BaseClass) {
     return BaseClass.extend({
       constructor: function(options) {
@@ -274,7 +274,7 @@ MixinBackbone = function(Backbone) {
         }
       },
       getViewDI: function(ViewClass, options) {
-        var TypeView, key;
+        var TypeView, diview, key, remove, removeFlag, view, _ref;
         if (options == null) {
           options = {};
         }
@@ -290,8 +290,20 @@ MixinBackbone = function(Backbone) {
           TypeView.prototype._$_di || (TypeView.prototype._$_di = _.uniqueId("_$_di"));
           key = ViewClass.key;
         }
-        if (this._$_p.diViews[key] == null) {
-          this._$_p.diViews[key] = new TypeView(options);
+        diview = this._$_p.diViews[key];
+        removeFlag = !!(diview != null ? (_ref = diview._$_p) != null ? _ref.removeFlag : void 0 : void 0);
+        if ((diview == null) || removeFlag) {
+          this._$_p.diViews[key] = view = new TypeView(options);
+          if (view._$_p == null) {
+            view._$_p = {
+              removeFlag: false
+            };
+            remove = view.remove;
+            view.remove = function() {
+              remove.apply(view, arguments);
+              return view._$_p.removeFlag = true;
+            };
+          }
         }
         return this._$_p.diViews[key];
       },
